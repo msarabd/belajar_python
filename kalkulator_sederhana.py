6fffb22ceb33fb5fe51fe4091606b75f222bfe92
Mahdi <msar.abdullah.idn@gmail.com>
2025-12-23 15:03:22 +0800

    prosedur pada kalkualtor

--- kalkulator_sederhana.py
+++ kalkulator_sederhana.py
@@ -1,6 +1,94 @@
 import os
 from prettytable import PrettyTable
 
+def pertambahan():
+    while True:
+        os.system("cls")
+        print(5*"=", "Pertambahan", 5*"=", "\n")
+
+        try:
+            angka_1 = float(input("Masukkan angka pertama = "))
+            angka_2 = float(input("Masukkan angka kedua = "))
+            hasil = angka_1 + angka_2
+            
+            if hasil.is_integer() :
+                print(f"Hasil dari {angka_1} ditambah {angka_2} adalah {int(hasil)}")
+
+            else:
+                print(f"Hasil dari {angka_1} ditambah {angka_2} adalah {round(hasil, 2)}")
+            
+            input("\n(Ketuk enter untuk kembali)")
+            break
+    
+        except Exception as e:
+            input(f"\n({e}, ketuk enter untuk kembali)")
+
+def pengurangan():
+    while True:
+        os.system("cls")
+        print(5*"=", "Pengurangan", 5*"=", "\n")
+
+        try:
+            angka_1 = float(input("Masukkan angka pertama = "))
+            angka_2 = float(input("Masukkan angka kedua = "))
+            hasil = angka_1 - angka_2
+            
+            if hasil.is_integer() :
+                print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {int(hasil)}")
+
+            else:
+                print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {round(hasil, 2)}")
+            
+            input("\n(Ketuk enter untuk kembali)")
+            break
+
+        except Exception as e:
+            input(f"\n({e}, ketuk enter untuk kembali)")
+
+def perkalian():
+    while True:
+        os.system("cls")
+        print(5*"=", "Perkalian", 5*"=", "\n")
+
+        try:
+            angka_1 = float(input("Masukkan angka pertama = "))
+            angka_2 = float(input("Masukkan angka kedua = "))
+            hasil = angka_1 * angka_2
+
+            if hasil.is_integer() :
+                print(f"Hasil dari {angka_1} dikali {angka_2} adalah {int(hasil)}")
+
+            else:
+                print(f"Hasil dari {angka_1} dikali {angka_2} adalah {round(hasil, 2)}")
+            
+            input("\n(Ketuk enter untuk kembali)")
+            break
+
+        except Exception as e:
+            input(f"\n({e}, ketuk enter untuk kembali)")
+
+def pembagian():
+    while True:
+        os.system("cls")
+        print(5*"=", "Pembagian", 5*"=", "\n")
+
+        try:
+            angka_1 = float(input("Masukkan angka pertama = "))
+            angka_2 = float(input("Masukkan angka kedua = "))
+            hasil = angka_1 / angka_2
+            
+            if hasil.is_integer() :
+                print(f"Hasil dari {angka_1} dibagi {angka_2} adalah {int(hasil)}")
+
+            else:
+                print(f"Hasil dari {angka_1} dibagi {angka_2} adalah {round(hasil, 2)}")
+
+            input("\n(Ketuk enter untuk kembali)")
+            break
+
+        except Exception as e:
+            input(f"\n({e}, ketuk enter untuk kembali)")
+
 while True:
     os.system("cls")
     tabel_menu = PrettyTable()
@@ -18,102 +106,20 @@ while True:
     pilihan_1 = input("Pilih menu (1-6) = ").strip()
     
     if pilihan_1 == "1":
-        while True:
-            os.system("cls")
-            print(5*"=", "Pertambahan", 5*"=", "\n")
-
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 + angka_2
-            
-            if hasil.is_integer() :
-                print(f"Hasil dari {angka_1} ditambah {angka_2} adalah {int(hasil)}")
-
-            else:
-                print(f"Hasil dari {angka_1} ditambah {angka_2} adalah {round(hasil, 2)}")
-            
-            input("\n(Tekan enter untuk kembali)")
-            break
+        pertambahan()
 
     elif pilihan_1 == "2":
-        while True:
-            os.system("cls")
-            print(5*"=", "Pengurangan", 5*"=", "\n")
-
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 - angka_2
-            
-            if hasil.is_integer() :
-                print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {int(hasil)}")
-
-            else:
-                print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {round(hasil, 2)}")
-            
-            input("\n(Tekan enter untuk kembali)")
-            break
+        pengurangan()
     
     elif pilihan_1 == "3":
-        while True:
-            os.system("cls")
-            print(5*"=", "Perkalian", 5*"=", "\n")
-
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 * angka_2
-
-            if hasil.is_integer() :
-                print(f"Hasil dari {angka_1} dikali {angka_2} adalah {int(hasil)}")
-
-            else:
-                print(f"Hasil dari {angka_1} dikali {angka_2} adalah {round(hasil, 2)}")
-            
-            input("\n(Tekan enter untuk kembali)")
-            break
+        perkalian()
     
     elif pilihan_1 == "4":
-        while True:
-            os.system("cls")
-            print(5*"=", "Pembagian", 5*"=", "\n")
+        pembagian()
 
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 / angka_2
-            
-            if hasil.is_integer() :
-                print(f"Hasil dari {angka_1} dibagi {angka_2} adalah {int(hasil)}")
-
-            else:
-                print(f"Hasil dari {angka_1} dibagi {angka_2} adalah {round(hasil, 2)}")
-
-            input("\n(Tekan enter untuk kembali)")
-            break
-    
-    elif pilihan_1 == "5":
-        while True:
-            os.system("cls")
-            print(5*"=", "Pengurangan", 5*"=", "\n")
-
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 - angka_2
-            print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {hasil}")
-            input("\n(Tekan enter untuk kembali)")
-            break
-
-    elif pilihan_1 == "6":
-        while True:
-            os.system("cls")
-            print(5*"=", "Pengurangan", 5*"=", "\n")
-
-            angka_1 = float(input("Masukkan angka pertama = "))
-            angka_2 = float(input("Masukkan angka kedua = "))
-            hasil = angka_1 - angka_2
-            print(f"Hasil dari {angka_1} dikurang {angka_2} adalah {hasil}")
-            input("\n(Tekan enter untuk kembali)")
-            break
-
-    else:
+    elif pilihan_1 == "0":
         break
 
-    #
+    else:
+        input("\n(Input tidak valid, ketuk enter untuk memilih kembali)")
+    
